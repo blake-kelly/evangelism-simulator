9cbb8cdeb830fdad26f830f89843a1087761e90b
Blake Kelly <BlakeKelly@Mac.attlocal.net>
2025-08-08 14:20:54 -0500

    Add basic Simulator component

---
+++ components/Simulator.js
@@ -0,0 +1,8 @@
+export default function Simulator() {
+  return (
+    <div style={{ padding: '20px', border: '1px solid #ccc' }}>
+      <h2>Evangelism Simulator</h2>
+      <p>This is where your interactive training will go.</p>
+    </div>
+  );
+}
