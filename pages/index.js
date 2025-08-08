4d18d824f498bf1a109d5bcfb8d938d2358e2758
Blake Kelly <BlakeKelly@Mac.attlocal.net>
2025-08-08 14:32:27 -0500

    Import and display Simulator on homepage

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import Simulator from "../components/Simulator";
+import Simulator from '../components/Simulator';
 
 export default function Home() {
   return (
     <div>
       <h1>Welcome to the Evangelism Simulator!</h1>
-      <p>Get started by launching the simulator component.</p>
+      <p>Get started by launching the simulator component below.</p>
       <Simulator />
     </div>
   );
