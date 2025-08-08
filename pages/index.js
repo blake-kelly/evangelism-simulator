8dbbe42a7370e74260c794b427f182d9a38aefdc
Blake Kelly <BlakeKelly@Mac.attlocal.net>
2025-08-08 14:10:26 -0500

    Use Simulator component on home page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
+import Simulator from "../components/Simulator";
+
 export default function Home() {
   return (
     <div>
       <h1>Welcome to the Evangelism Simulator!</h1>
       <p>Get started by launching the simulator component.</p>
+      <Simulator />
     </div>
   );
 }
